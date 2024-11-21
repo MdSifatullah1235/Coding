d5499a8340559aafa9c6c9eb7108f1ddc3ee9586
MdSifatullah1235 <miasifat1235@gmail.com>
2024-11-21 19:18:55 +0600

    version 1

--- OOP/Project4.py
+++ OOP/Project4.py
@@ -2,4 +2,12 @@ class StringIO():
     def __init__(self):
         self.str1 = ""
 
-    def getSting(self):
+    def getString(self):
+        self.str1 = input("Enter a string :")
+    
+    def result(self):
+        print("Converted String :",self.str1.upper())
+
+obj = StringIO()
+obj.getString()
+obj.result()
