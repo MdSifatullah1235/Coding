be52042b4ca2c49a34ca5034b1fb3424519966c6
MdSifatullah1235 <miasifat1235@gmail.com>
2024-12-05 17:51:06 +0600

    version 1

---
+++ Abstraction and Polymorphism/ACProject1.py
@@ -0,0 +1,13 @@
+class BMW:
+    def speed(self):
+        return "BMW: 240 km/h"
+
+class Ferrari:
+    def speed(self):
+        return "Ferrari: 350 km/h"
+
+bmw = BMW()
+ferrari = Ferrari()
+
+print(bmw.speed())     
+print(ferrari.speed()) 
