4e9c2350b407701e4b8bbfb18c505f8ace31d9b7
MdSifatullah1235 <miasifat1235@gmail.com>
2024-12-12 19:18:13 +0600

    version 1

--- Pygame/Project1.py
+++ Pygame/Project1.py
@@ -8,6 +8,6 @@ done = False
 
 while not done:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             pygame.quit()
 pygame.display.flip()
