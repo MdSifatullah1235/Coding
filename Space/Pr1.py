3c4b01481d7d26f9e0415bf78c90bb866c6849ab
MdSifatullah1235 <miasifat1235@gmail.com>
2024-12-24 19:46:34 +0600

    version 1

--- Space/Pr1.py
+++ Space/Pr1.py
@@ -17,3 +17,36 @@ COLLISION_DISTANCE = 27
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 bg = pygame.image.load("SpaceBG.jpg")
+
+playerIMG = pygame.image.load("Player.png")
+icon = pygame.image.load("ufo.png")
+pygame.display.set_caption("Space Invaders")
+
+pygame.display.set_icon(icon)
+
+playerX = PLAYER_STRAT_X
+playerY = PLAYER_STRAT_Y
+
+player_x_change = 0
+
+enemyIMG = []
+enemyX = []
+enemyY = []
+enemy_x_change = []
+enemy_y_change = []
+num_enemies = 6
+
+for i in range(num_enemies):
+    enemyIMG.append(pygame.image.load("Enemy.png"))
+    enemyX.append(random.randint(0, SCREEN_WIDTH - 64))
+    enemyY.append(random.randint(ENEMY_START_YMIN,ENEMY_START_YMAX))
+    enemy_x_change.append(ENEMY_SPEED_X)
+    enemy_y_change.append(ENEMY_START_Y)
+
+bulletIMG = pygame.image.load("Bullet.png")
+bulletX = 0
+bulletY = PLAYER_STRAT_Y
+bulletx_change  = 0
+bullety_change = BULLET_SPEED_Y
+bullet_state = "ready"
+
