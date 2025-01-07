666a7d2b41c09f4790b738308e89b13d962fc688
MdSifatullah1235 <miasifat1235@gmail.com>
2025-01-07 19:20:55 +0600

    version 1

--- Where’stheevent/Project1.py
+++ Where’stheevent/Project1.py
@@ -4,17 +4,16 @@ window.geometry("100x100")
 window.title("Event Handler")
 
 def handle_keypress(event):
-    print(f"Key Pressed {event.char}")
+    print(f"Key Pressed: {event.char}")
 
 window.bind("<Key>", handle_keypress)
 
 def handle_click(event):
-    print(f"\n Button Is Clicked")
-
+    print("Button is clicked")
 
 button = Button(text="Click Me")
-
 button.pack()
-button.bind("Button-1",handle_click)
+button.bind("<Button-1>", handle_click)
 
 window.mainloop()
+
