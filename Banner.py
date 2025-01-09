240abd285478631523f7c12ff42f0b08c34352ea
MdSifatullah1235 <miasifat1235@gmail.com>
2025-01-09 13:55:13 +0600

    version 1

---
+++ Banner.py
@@ -0,0 +1,9 @@
+channel_name = "{(𝙽𝚘𝚗𝚎)}"
+channel_handle = "𝙽𝚘𝚗𝚎_𝚃𝚢𝚙𝚎"
+channel_desc = "𝚓𝚞𝚜𝚝 𝚙𝚘𝚜𝚝𝚒𝚗𝚐 𝚛𝚊𝚗𝚍𝚘𝚖 𝚜𝚝𝚞𝚏𝚏" 
+mission = None
+
+print(channel_name)
+print(channel_handle)
+print(channel_desc)
+print(mission)
