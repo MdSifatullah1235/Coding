a4a52d72ec17aa2e8146ebfa45af130e3fcc6ac2
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-06 19:08:51 +0600

    v1

---
+++ Java/Addition/Addition.java
@@ -0,0 +1,10 @@
+package Java.Addition;
+// just some simple additions 
+public class Addition {
+    public static void main(String[] args) {
+    System.out.print("1 + 1 = ");
+    System.out.println(1 + 1);
+    System.out.print("5 + 7 = ");
+    System.out.println(5 + 7);
+  }
+}
