96211cf6d3da9f0f3b0209dbad6aa03cdbe5bf79
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-07 19:17:15 +0600

    v1

---
+++ Java/DtypesOps/Maggie.java
@@ -0,0 +1,12 @@
+package Java.DtypesOps;
+
+public class Maggie {
+    public static void main(String[] args) {
+        System.out.println("Maggie Receipe");
+        Receipe();
+    }
+    public static void Receipe() {
+        System.out.println("Boil water in pan");
+        System.out.println("Add the maggie receipe after this");
+    }
+}
