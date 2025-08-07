24de3d5d31dea31f01cfae888580e4ec18310a6f
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-07 22:35:39 +0600

    v1

--- Java/LeapYearChecker/Code.java
+++ Java/LeapYearChecker/Code.java
@@ -2,7 +2,7 @@ package Java.LeapYearChecker;
 
 public class Code {
     public static void main(String[] args) {
-        int year = 1900;
+        int year = 2048;
         String checker = (year % 4 == 0 && (year % 100 != 0)) ? "This is a leap year" : "This is not a leap year";
         System.out.println(checker);
     }
