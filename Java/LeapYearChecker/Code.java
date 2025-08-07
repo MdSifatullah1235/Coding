073eaf99c0b76835db0115ffd265199f70971fb6
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-07 22:40:23 +0600

    v1

--- Java/LeapYearChecker/Code.java
+++ Java/LeapYearChecker/Code.java
@@ -1,8 +1,14 @@
 package Java.LeapYearChecker;
+/* Just a program to check 
+if a year assigned to the "year" variable
+is a leap year or not
+*/
 
 public class Code {
     public static void main(String[] args) {
+        System.out.println("========= Leap Year Checker =========");
         int year = 2048;
+        System.out.println("Year = " + year);
         String checker = (year % 4 == 0 && (year % 100 != 0)) ? year + " is a leap year" : year + " is not a leap year";
         System.out.println(checker);
     }
