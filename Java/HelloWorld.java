b51af439e623b5ac218fb06c5a079d1a0277543d
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-08 14:26:34 +0600

    v1

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -4,5 +4,5 @@ public class HelloWorld {
 public static void main(String[] args) {
   System.out.println("Hello World");
 }
-
+// im writing this in comments but codeium is there a short cut for system.out.println ?? answer me please :)   are you there ??  
 }
