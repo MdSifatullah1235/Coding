bcdfe4727341403ec638d5532b05936a82e505d2
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-08 17:12:30 +0600

    v1

---
+++ Java/ConStructAraay/Forloops.java
@@ -0,0 +1,13 @@
+package Java.ConStructAraay;
+public class Forloops {
+public static void main(String[] args) {
+for (int i = 10; i > 1; i--) {
+System.out.println(i);
+}
+int j = 0;
+while (j < 11) {
+System.out.println(j);
+j++;
+}
+}
+}
