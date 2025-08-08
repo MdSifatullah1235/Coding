566b3e13f0fb0bb766b6e4ad7c709d51c52463e0
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-08 17:16:39 +0600

    v1

---
+++ Java/ConStructAraay/HelloUser.java
@@ -0,0 +1,13 @@
+package Java.ConStructAraay;
+import java.util.*;
+
+public class HelloUser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String name = sc.nextLine();
+        System.out.println( "Enter youre lucky number : ");
+        int number = sc.nextInt();
+        System.out.println("Hello " + name + " your lucky number is " + number);
+}
+}
