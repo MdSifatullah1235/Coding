ba013660ffa805a0a240c848dbd64c9d014d2252
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-08 17:41:06 +0600

    v1

---
+++ Java/ConStructAraay/GradingSytem.java
@@ -0,0 +1,59 @@
+package Java.ConStructAraay;
+import java.util.Scanner;
+
+import Java.ConStructAraay.*;
+public class GradingSytem {
+    public static void main(String[] args) {
+        int sum = 0;
+        String result;
+        Scanner sc = new Scanner(System.in);
+        int noOfSubs = sc.nextInt();
+        int marks[] = new int[noOfSubs];
+        System.out.println("Enter the marks of "+ noOfSubs + "Press enter to give marks for another subject : ");
+        for(int i = 0; i < noOfSubs; i++) {
+            marks[i] = sc.nextInt();
+        
+}
+for(int j =0; j < noOfSubs; j ++) {
+    sum += marks[j];
+}
+
+int percentage = sum / noOfSubs;
+System.out.println("Your percentage is " + percentage);
+if (percentage >= 95){
+    result = "Your grade is A+";
+}
+else if (percentage >= 90) {
+    result = "Your grade is A";
+}
+else if (percentage >= 85) {
+    result = "Your grade is B+";
+} 
+else if (percentage >= 80) {
+    result = "Your grade is B";
+} 
+else if (percentage >= 75) {
+    result = "Your grade is C+";
+} 
+else if (percentage >= 70) {
+    result = "Your grade is C";
+} 
+else if (percentage >= 65) {
+    result = "Your grade is D+";
+} 
+else if (percentage >= 60) {
+    result = "Your grade is D";
+} 
+else if (percentage >= 55) {
+    result = "Your grade is F+";
+}
+else if (percentage >= 50) {
+    result = "Your grade is F";
+}
+else {
+    result = "You failed";
+}
+
+System.out.println(result);
+}
+}
