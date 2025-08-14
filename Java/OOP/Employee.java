41c7076e0469650b2ee82585322405cd8d39a0c2
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-14 19:11:33 +0600

    v1

--- Java/OOP/Employee.java
+++ Java/OOP/Employee.java
@@ -14,4 +14,15 @@ public class Employee {
     void displaydetails(){
         System.out.println(empno+ " | " + name + " | " + salary);
     }
+class Main {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee();
+        Employee emp2 = new Employee();
+        Employee emp3 = new Employee();
+
+        emp1.displaydetails();
+        emp2.displaydetails();
+        emp3.displaydetails();
+}
+}
 }
