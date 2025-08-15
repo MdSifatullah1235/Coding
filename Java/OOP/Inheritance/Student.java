613faa0e3d74b9dfb908b0a8121a373b6ca4e03a
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-15 15:14:20 +0600

    v1

--- Java/OOP/Inheritance/Student.java
+++ Java/OOP/Inheritance/Student.java
@@ -1,5 +1,23 @@
 package Java.OOP.Inheritance;
 
 public class Student {
-    
+    static class Parent {
+        int age, id;
+        String name;
+        void naming(String name) {
+            System.out.println("My name is " + name);
+        }
+    }
+    static class Child extends Parent {
+        void ageN(int age) {
+            System.out.println("My age is " + age);
+        }
+    }
+    static class Main {
+        public static void main(String[] args) {
+            Child s = new Child();
+            s.naming("Sifat");
+            s.ageN(14);
+        }
+    }
 }
