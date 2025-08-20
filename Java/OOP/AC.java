b6e20afa5d080aad593503b20af641b90f9658ed
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-20 16:14:17 +0600

    v1

--- Java/OOP/AC.java
+++ Java/OOP/AC.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class AC {
+    String name;
+    int[] marks = new int[5];
+    String[] subjects = {"Bangla","English","Math","B.G.S","Science"};
     
+    Student(){
+        System.out.println("*****");
+        empno = 101;
+        name = "Asish";
+        salary = 10000f;
+}
+    void displaydetails(){
+        System.out.println(empno+ " | " + name + " | " + salary);
+    }
+class Main {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee();
+        Employee emp2 = new Employee();
+        Employee emp3 = new Employee();
+
+        emp1.displaydetails();
+        emp2.displaydetails();
+        emp3.displaydetails();
+}
+}
 }
