2addc101c8ac7eb5cc2359a963165a71efda4c0e
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-21 19:14:05 +0600

    v1

---
+++ Java/OOP/Access/Super1.java
@@ -0,0 +1,22 @@
+package Access;
+
+public class Super1 {
+    public static void main(String[] args) {
+        subclass obj = new subclass();
+        obj.display();
+    }
+}
+
+
+class supersclass {
+    int number = 10;
+}
+
+
+class subclass extends supersclass {
+    int number = 20;
+    void display() {
+        System.out.println(number);
+        System.out.println(super.number);
+    }
+}
