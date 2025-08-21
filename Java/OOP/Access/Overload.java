cc819b970492b20a761064db4324b089c6c0fcae
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-21 18:56:50 +0600

    v1

---
+++ Java/OOP/Access/Overload.java
@@ -0,0 +1,37 @@
+package Access;
+
+public class Overload {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student(30, "Sifat");
+        Student s3 = new Student(30, "Sifat", 1000);
+
+        s1.displayDetails();
+        s2.displayDetails();
+        s3.displayDetails();
+    }
+}
+
+class Student {
+    int id;
+    String name;
+    float stipend;
+
+    Student() {}
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    Student(int id, String name, float stipend) {
+        this.id = id;
+        this.name = name;
+        this.stipend = stipend;
+
+    }
+
+    void displayDetails(){
+        System.out.println(id + " | " + name + " | " + stipend);
+    }
+}
