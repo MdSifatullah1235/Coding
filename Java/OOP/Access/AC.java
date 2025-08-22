c219a0e787c61abd38d82b7b8f8d8eb7cfdf97cf
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-22 14:31:18 +0600

    v1

---
+++ Java/OOP/Access/AC.java
@@ -0,0 +1,18 @@
+package Access;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AC {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the size of the array : ");
+    int size = sc.nextInt();
+    System.out.print("Enter the "+ 5 +"elements of the array : ");
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+}
+    System.out.println("Array : " + Arrays.toString(arr));
+    sc.close();
+}
+}
