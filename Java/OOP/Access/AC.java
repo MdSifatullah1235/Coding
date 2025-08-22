f8f629b3a96224968dfd52bf7107c716131e34e8
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-22 15:04:01 +0600

    v1

--- Java/OOP/Access/AC.java
+++ Java/OOP/Access/AC.java
@@ -7,7 +7,7 @@ public class AC {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter the size of the array : ");
     int size = sc.nextInt();
-    System.out.print("Enter the "+ 5 +"elements of the array : ");
+    System.out.print("Enter the "+ size +" elements of the array : ");
     int[] arr = new int[size];
     for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
