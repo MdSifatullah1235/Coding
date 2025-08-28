6c2ce86f5c3e285c7f4fc2569bff31e48fe8496b
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-28 18:54:00 +0600

    v1

--- Java/OOP/Encapsulation/AC.java
+++ Java/OOP/Encapsulation/AC.java
@@ -6,10 +6,14 @@ public class AC {
         Cuboid cuboid = new Cuboid(2, 2, 2);
         Cylinder cylinder = new Cylinder(2, 2);
         Trianguler trianguler = new Trianguler(2, 2, 2);
-        System.out.println(cube.getVolume());
-        System.out.println(cuboid.getVolume());
-        System.out.println(cylinder.getVolume());
-        System.out.println(trianguler.getVolume());
+        Cone cone = new Cone(2, 2);
+        Sphere sphere = new Sphere(2);
+        System.out.println("Volume of a Cube : " + cube.getVolume());
+        System.out.println("Volume of a Cuboid : " + cuboid.getVolume());
+        System.out.println("Volume of a Cylinder : " + cylinder.getVolume());
+        System.out.println("Volume of a Trianguler : " + trianguler.getVolume());
+        System.out.println("Volume of a Cone : " + cone.getVolume());
+        System.out.println("Volume of a Sphere : " + sphere.getVolume());
     }
 }
 
@@ -63,5 +67,26 @@ class Trianguler extends Shape {
     }
     public double getVolume() {
         return 0.5 * baseEdge * height * length;
+    }
+}
+
+class Cone extends Shape {
+    private double radius, perpendicularHeight;
+    public Cone(double radius, double height) {
+        this.radius = radius;
+        this.perpendicularHeight = height;
+    }
+    public double getVolume() {
+        return 0.33 * 3.14 * radius * radius * perpendicularHeight;
+    }
+}
+
+class Sphere extends Shape {
+    private double radius;
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+    public double getVolume() {
+        return 1.33 * 3.14 * radius * radius * radius;
     }
 }
