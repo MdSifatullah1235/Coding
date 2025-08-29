e6343711417e38a332f38e20834cdf7497db1006
MdSifatullah1235 <miasifat1235@gmail.com>
2025-08-29 15:22:24 +0600

    v1

--- Java/OOP/Interfaces/Vehicles.java
+++ Java/OOP/Interfaces/Vehicles.java
@@ -1,7 +1,23 @@
 package Interfaces;
 
 public class Vehicles {
-    
+    public static void main(String[] args) {
+        Bicycle bicycle = new Bicycle();
+        bicycle.changeGear(2);
+        bicycle.speedUp(3);
+        bicycle.applyBrakes(1);
+
+        System.out.println("The present state of bicycle : ");
+        bicycle.printStates();
+
+        Car car = new Car();
+        car.changeGear(1);
+        car.speedUp(4);
+        car.applyBrakes(3);
+
+        System.out.println("The present state of car : ");
+        car.printStates();
+    }
 }
 
 interface Vehicle {
@@ -32,7 +48,30 @@ class Bicycle implements Vehicle {
     public void printStates() {
         System.out.println("Speed: " + speed);
         System.out.println("Gear: " + gear);
-        
+
     }
 
+}
+
+class Car implements Vehicle {
+    int speed;
+    int gear;
+    @Override
+    public void changeGear(int newGear) {
+        gear = newGear;
+    }
+    @Override
+    public void speedUp(int increment) {
+        speed += increment;
+    }
+    @Override
+    public void applyBrakes(int decrement) {
+        speed -= decrement;
+    }
+
+    public void printStates() {
+        System.out.println("Speed: " + speed);
+        System.out.println("Gear: " + gear);
+
+    }
 }
