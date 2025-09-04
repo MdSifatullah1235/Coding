29c1ccd801bc0cef39ce40bab08c87534d0634cd
MdSifatullah1235 <miasifat1235@gmail.com>
2025-09-04 18:45:38 +0600

    v1

---
+++ Java/ExceptionalHandling/Application.java
@@ -0,0 +1,22 @@
+package ExceptionalHandling;
+import java.util.*;
+public class Application {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Enter 2 numbers : ");
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = x / y;
+            System.out.println(x  + " / " + y + " = " + z);
+        } catch (ArithmeticException ex) {
+            System.out.println("------ Catch block ------");
+            System.out.println(ex.toString());
+        } finally {
+            System.out.println("------ Finally block ------");
+            System.out.println("Application designed and developed by @ Codingal");
+            sc.close();
+        }
+        System.out.println("------ Done ------");
+    }
+}
