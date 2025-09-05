e90d30ded09ba9351243adec7f393dc10af882e8
MdSifatullah1235 <miasifat1235@gmail.com>
2025-09-05 15:11:45 +0600

    v1

--- Java/WingardiumLeviosa/Magic2.java
+++ Java/WingardiumLeviosa/Magic2.java
@@ -3,12 +3,16 @@ import java.util.*;
 public class Magic2 {
     public static void main(String[] args) {
         System.out.println("Lambdas");
+        // Created a integer array
         ArrayList<Integer> numbers = new ArrayList<>();
+        // Added 4 elements
         numbers.add(43);
         numbers.add(58);
         numbers.add(65);
         numbers.add(98);
+        // Displayed the elements
         numbers.forEach((x) -> { System.out.println(x); });
-        numbers.forEach(n -> {if (n == 65) {System.out.println("Found" + n);}});
+        // Finding a specific element
+        numbers.forEach(n -> {if (n == 65) {System.out.println("Found " + n);}});
     }
 }
