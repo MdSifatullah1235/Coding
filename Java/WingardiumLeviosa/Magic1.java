16fd057c223744fc69eab5e7214383f741128cd6
MdSifatullah1235 <miasifat1235@gmail.com>
2025-09-05 15:01:34 +0600

    v1

--- Java/WingardiumLeviosa/Magic1.java
+++ Java/WingardiumLeviosa/Magic1.java
@@ -2,8 +2,9 @@ package WingardiumLeviosa;
 import java.util.*;
 public class Magic1 {
     public static void main(String[] args) {
+        // ArrayList
         ArrayList<String> animals = new ArrayList<>();
-
+        // added 3 elements
         animals.add("Lion");
         animals.add("Cheetah");
         animals.add("Tiger");
@@ -18,5 +19,15 @@ public class Magic1 {
         else{
             System.out.println("Array is not empty");
         }
+
+        animals.add("Lion");
+        animals.add("Cheetah");
+        animals.add("Tiger");
+
+        System.out.println("Size of the array : " + animals.size());
+        System.out.println("Adding more elements to the array : " + animals.add("Horse") + " " + animals.add("Elephant"));
+        System.out.println(animals);
+        animals.remove(2);
+        System.out.println(animals);
     }
 }
