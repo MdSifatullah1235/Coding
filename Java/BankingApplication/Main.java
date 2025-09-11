31f4afc9eb189b912af542511f93c022c2687abe
MdSifatullah1235 <miasifat1235@gmail.com>
2025-09-11 18:42:36 +0600

    v1

---
+++ Java/BankingApplication/Main.java
@@ -0,0 +1,19 @@
+package BankingApplication;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        boolean infiniteLoop = true;
+        double[] accountBalance = new double[1000];
+        String[] accountName = new String[1000];
+        int option, size = 100;
+        while (infiniteLoop) {
+            System.out.println("Welcome to Codingal Banking Services");
+            System.out.println("Banking Menu :");
+            System.out.println("1 -> Add Account");
+            System.out.println("2 -> Change Customer Name");
+            System.out.println("3 -> Check Account Balance");
+            System.out.println("4 -> Update Account Balance");
+            
+        }
+    }
+}
