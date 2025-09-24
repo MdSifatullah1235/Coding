e2b4fad2688072e5db320153cf256e1745172d82
MdSifatullah1235 <miasifat1235@gmail.com>
2025-09-24 19:18:05 +0600

    v1

--- Python/AI in Games/AC.py
+++ Python/AI in Games/AC.py
@@ -5,14 +5,13 @@ init(autoreset=True)
 def display_ground():
     print()
 
-    print(Fore.GREEN + f"             Player                 ")
+    print(Fore.GREEN + f"              Player               ")
     print(Fore.CYAN + f"           Player chose              ")
     print("")
     print("-------------------------------")
-    print(Fore.RED + f"             AI                 ")
+    print(Fore.RED + f"              AI                 ")
     print(Fore.CYAN + f"           AI chose              ")
     print("")
-    print("-------------------------------")
 
 
 display_ground()
