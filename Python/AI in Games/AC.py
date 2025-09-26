4eaf91bd1a5b7175139cadba0a399c276fdd316d
MdSifatullah1235 <miasifat1235@gmail.com>
2025-09-26 14:32:02 +0600

    v1

--- Python/AI in Games/AC.py
+++ Python/AI in Games/AC.py
@@ -2,21 +2,112 @@ import random
 from colorama import Fore, init, Style
 init(autoreset=True)
 
-def display_board(board):
-    print()
-    def colored(cell):
-        if cell == "X":
-            return Fore.RED + cell + Style.RESET_ALL
-        elif cell == "O":
-            return Fore.BLUE + cell + Style.RESET_ALL
-        else:
-            return Fore.YELLOW + cell + Style.RESET_ALL
-    
-    print(" " + colored(board[0]) + " | " + colored(board[1]) + " | " + colored(board[2]))
-    print(Fore.CYAN + "-----------")
-    print(" " + colored(board[3]) + " | " + colored(board[4]) + " | " + colored(board[5]))
-    print(Fore.CYAN + "-----------")
-    print(" " + colored(board[6]) + " | " + colored(board[7]) + " | " + colored(board[8]))
-    print()
+player_choice = input("Enter your move (rock, paper, scissors): ").lower()
 
-display_board(["1","2","3","4","5","6","7","8","9"])
+history = []
+
+def playground(ai_choice):
+    print()
+    print(Fore.GREEN + "--- Player ---")
+    print(Fore.CYAN + f"Player chose {player_choice}")
+    if player_choice == "rock":
+        print(Fore.GREEN + """
+    _______
+---'   ____ )
+      (_____ )
+      (_____ )
+      (____ )
+---.__(___ )
+""")
+    elif player_choice == "paper":
+        print(Fore.GREEN + """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+    elif player_choice == "scissors":
+        print(Fore.GREEN + """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+
+    print()
+    print("----------------------------")
+    print()
+    print(Fore.CYAN + f"AI chose {ai_choice}")
+    if ai_choice == "rock":
+        print(Fore.RED + """
+    _______
+---'   ____ )
+      (_____ )
+      (_____ )
+      (____ )
+---.__(___ )
+""")
+    elif ai_choice == "paper":
+        print(Fore.RED + """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""")
+    elif ai_choice == "scissors":
+        print(Fore.RED + """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+""")
+    print(Fore.RED + "--- AI ---")
+
+
+def ai_move():
+    if not history:  
+        return "paper"
+    else:
+        last = history[-1]
+
+        if (last == "rock" and player_choice == "rock") or \
+           (last == "paper" and player_choice == "paper") or \
+           (last == "scissors" and player_choice == "scissors"):
+            if player_choice == "rock":
+                return "paper"
+            elif player_choice == "paper":
+                return "scissors"
+            else:
+                return "rock"
+
+        if last != player_choice:
+            if player_choice == "rock":
+                return "paper"
+            elif player_choice == "paper":
+                return "scissors"
+            else:
+                return "rock"
+
+        return random.choice(["rock", "paper", "scissors"])
+
+
+ai_choice = ai_move()
+history.append(player_choice)
+playground(ai_choice)
+
+if player_choice == ai_choice:
+    print(Fore.YELLOW + "It's a tie!")
+elif (player_choice == "rock" and ai_choice == "scissors") or \
+     (player_choice == "paper" and ai_choice == "rock") or \
+     (player_choice == "scissors" and ai_choice == "paper"):
+    print(Fore.GREEN + "You win!")
+else:
+    print(Fore.RED + "AI wins!")
