ac6695b9d98a8a7f189be947a0fb5c7dfb6e9f85
MdSifatullah1235 <miasifat1235@gmail.com>
2025-10-03 09:18:57 +0600

    v1

--- Python/Image Manipulation/1.py
+++ Python/Image Manipulation/1.py
@@ -1,8 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread('Example.png')
-
+img = cv2.imread('C:\Users\User\OneDrive\Desktop\Coding\Python\Image Manipulation\Example.png')
 #Convert BGR to RGB
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 plt.imshow(img_rgb)
